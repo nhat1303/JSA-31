d17925d2084d6c0b0cb9eea4556a43b535d78cb2
nhat1303 <nhathahaha13@gmail.com>
2022-11-26 08:11:36 +0700

    JSA31

---
+++ homework/Day_1/baitap1.js
@@ -0,0 +1,74 @@
+// let a = Number(window.prompt("Nhập số 1"))
+// let b= Number(window.prompt("Nhập số 2 :"));
+
+// function abc(a, b){
+//     return a + b
+// }
+
+// console.log(abc(a,b))
+
+
+
+
+// var mang = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17, 18, 19, 20];
+
+// function abc(mang){
+//     let tong=0;
+//     for ( i =0; i< mang.length; i++){
+//         if(mang[i] % 2 ==0){
+//             tong+= mang[i]
+//         }
+       
+//     }
+//     return tong;
+// }
+
+// console.log(abc(mang))
+
+
+
+
+// let a = Number(window.prompt("Nhập số n : "))
+// function abc(a){
+//     let tong=0;
+//     if (a < 0){
+//         window.alert("n lớn hơn 0")
+//     } else {
+//         for(i=1; i<= a; i++){
+//             tong+= (i/a)
+//         }
+//     }
+//     return tong
+// }
+// console.log(abc(a))
+
+
+
+// let a = Number(window.prompt("Nhấp số cần bình phương : "))
+// function abc(a){
+//    return a*a
+// }
+
+// console.log("Số bình phương : " + abc(a))
+
+
+// let a = window.prompt("Nhập 1 chuỗi")
+
+// function abc(a){
+//     return a.toUpperCase(1)
+// }
+// console.log(abc(a))
+
+// let number = [100,500,167,565,5,139,-3,-100 ];
+
+// function abc(number){
+//     let min = number[0]
+//     for(i=1; i<number.length; i++){
+//         if( min > number[i]){
+//             min = number[i]
+//         }
+//     }
+//     return min
+    
+// }
+// console.log(abc(number))
---
+++ onclass/Day3/index3.js
@@ -0,0 +1,10 @@
+document.getElementById("showmore").addEventListener("click",
+function abc(){
+    if(document.getElementById("box").style.display =='none'){
+    document.getElementById("box").style.display ='block'}
+    else{
+        document.getElementById("box").style.display="none"
+    }
+}
+)
+

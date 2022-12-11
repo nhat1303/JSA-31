cbb59ea477c65147b6426559317d07902be27692
nhat1303 <nhathahaha13@gmail.com>
2022-12-11 18:37:09 +0700

    update

--- checkpoint_day4/index.js
+++ checkpoint_day4/index.js
@@ -1,22 +1,22 @@
-let number = Number(window.prompt("Nhập 1 số bất kì từ 4-20: "))
-let day_so = [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-function findEven(number,day_so){
-    for (i=0; i<=day_so.length; i++){
-        if(day_so[i]%2==0 & day_so[i] <= number){
-            return "các số chẵn" +day_so[i] 
-        }
-    }
-}
-console.log(findEven(number,day_so))
+// let number = Number(window.prompt("Nhập 1 số bất kì từ 4-20: "))
+// let day_so = []
+// function findEven(number,day_so){
+//     for (i=0; i<=number ; i++){
+//         if(i%2==0 & i <= number){
+//             return "các số chẵn" +  day_so.push(i)
+//         }
+//     }
+// }
+// console.log(findEven(day_so))
 
-    let rows=5;
-    for (i=1; i<=rows;i++){
-        let str =' ';
-        for(let j=1; j<=i ;j++){
-            str += "1"
-        }
-        console.log(str)
-    }
+//     let rows=5;
+//     for (i=1; i<=rows;i++){
+//         let str =' ';
+//         for(let j=1; j<=i ;j++){
+//             str += "1"
+//         }
+//         console.log(str)
+//     }
 
 
 
